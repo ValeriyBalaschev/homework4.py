5b720d97d95b1c8f7344abb040cad5b894e4309a
ValeriyBalaschev <bwengs.ru@mail.ru>
2024-07-01 00:22:03 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Организация программ и методы строк."
+my_string = input('Введите любую строку: ')
+print('в введенной строке ', len(my_string), 'символ/-ов/')
+print(my_string.upper())
+print(my_string.lower())
+my_string = my_string.replace(' ', '')
+print(my_string)
+print(my_string[0])
+print(my_string[-1])
